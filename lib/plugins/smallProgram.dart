87f8199ff980f5d1b7c5137e054b39a08b020bd8
Abner <0x23fd5097@gmail.com>
2023-07-10 23:13:22 +0800

    feat: 新增小程序api代理支持

--- lib/plugins/smallProgram.dart
+++ lib/plugins/smallProgram.dart
@@ -1,4 +1,5 @@
 /// H5小程序
+import 'dart:async';
 import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:archive/archive.dart';
@@ -7,6 +8,7 @@ import 'package:path/path.dart' as path;
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_static/shelf_static.dart'; // shelf静态文件中间件
+import 'package:shelf_proxy/shelf_proxy.dart'; // shelf代理中间件
 
 enum Status { // 状态
   success,
@@ -21,7 +23,7 @@ enum Progress { // 进度
 
 typedef void DecompressionCallback(Progress state);
 
-abstract class ProgramType{
+abstract class ProgramType {
   final String name; // 小程序名称
   final Uri serverAddress; // 静态服务器地址(远程下载服务器)
   ProgramType(this.name, this.serverAddress); // 小程序名称
@@ -48,7 +50,7 @@ class SmallProgram implements ProgramType{
 
   Future<Directory> get applicationDir => getApplicationDocumentsDirectory(); // 应用程序包目录(沙盒目录，读写无需单独的权限申请)，该文件夹一定存在
   Future<List<FileSystemEntity>> get programEntities => programDir.then((Directory dir) => dir.listSync(recursive:true, followLinks: false)); // 小程序静态资源文件信息
-  Future<Directory> get programDir async{ // 小程序资源存放目录
+  Future<Directory> get programDir async { // 小程序资源存放目录
     final Directory _dir = await applicationDir.then((Directory dir) => Directory(path.join(dir.path, name)));
 
     if (!_dir.existsSync()) await handleDecompression(); // 在获取该文件夹时，检测是否存在文件夹，不存在执行同步创建
@@ -56,13 +58,13 @@ class SmallProgram implements ProgramType{
     return _dir;
   }
 
-  Future<Directory> get packageDir async{ // 小程序下载(|存放)目录
+  Future<Directory> get packageDir async { // 小程序下载(|存放)目录
     final Directory _dir = await applicationDir.then((Directory dir) => Directory(path.join(dir.path, 'download')));
     if (!_dir.existsSync()) _dir.createSync(recursive: true); // 在获取该文件夹时，检测是否存在文件夹，不存在执行同步创建
     return _dir;
   }
 
-  Future<File> get packageFile async{ // 小程序原始文件
+  Future<File> get packageFile async { // 小程序原始文件
     final String packageFileName = '$name.zip';
     final String downloadPath = 'download';
     final File _file = await packageDir.then((Directory dir) => File(path.join(dir.path, packageFileName)));
@@ -77,7 +79,7 @@ class SmallProgram implements ProgramType{
     return _file;
   }
 
-  Future<void> handleDecompression([String _path]) async{ // 解压小程序压缩包
+  Future<void> handleDecompression([String _path]) async { // 解压小程序压缩包
     defaultDecompression(Progress.start);
     final File _compressFile = _path != null && _path.isNotEmpty ? File(_path) : (await packageFile); // 压缩文件
     final List<int> _bytes = _compressFile.readAsBytesSync();
@@ -86,7 +88,7 @@ class SmallProgram implements ProgramType{
     final Archive archive = ZipDecoder().decodeBytes(_bytes);
 
     print('解压资源存放路径: ${_programDir.path}');
-    archive.forEach((ArchiveFile _file){ // 将Zip存档的内容解压到磁盘。
+    archive.forEach((ArchiveFile _file) { // 将Zip存档的内容解压到磁盘。
       print('解压文件详细信息: $_file');
       if (_file.isFile) {
         File(path.join(_programDir.path, _file.name))
@@ -102,7 +104,7 @@ class SmallProgram implements ProgramType{
     defaultDecompression(Progress.finish);
   }
 
-  Future<void> handleDelete() async{
+  Future<void> handleDelete() async {
     final _programDir = await programDir;
     final _packageFile = await packageFile;
 
@@ -110,16 +112,22 @@ class SmallProgram implements ProgramType{
     _programDir.deleteSync(recursive: true); // 删除小程序资源存放目录
   }
 
-  Future<Uri> run() async{
+  shelf.Handler apiProxyHandler(innerHandler) => (shelf.Request request) { // 代理请求（后端服务）
+    if (request.url.path.startsWith('api/')) return proxyHandler(serverAddress.toString())(request); // 代理请求(/api开头的请求)
+    return innerHandler(request); // 非代理请求
+  };
+
+  Future<Uri> run() async {
     final InternetAddress host = InternetAddress.loopbackIPv4/*'127.0.0.1'*/; // 本机IP地址
     final int port = 8098; // 服务端口号
 
     final handler = const shelf
         .Pipeline()
         .addMiddleware(shelf.logRequests()) // 打印信息
+        .addMiddleware(apiProxyHandler) // 代理请求
         .addHandler(createStaticHandler(path.join((await programDir).path), defaultDocument: 'index.html')); // 静态文件中间件
 
-    return io.serve(handler, host, port).then((HttpServer _server){
+    return io.serve(handler, host, port).then((HttpServer _server) {
       server = _server; // 服务对象保存
       final Uri serverAddress = Uri( // 小程序服务运行服务地址
         scheme: 'http',
@@ -133,8 +141,8 @@ class SmallProgram implements ProgramType{
     });
   }
 
-  Future close(){
-    return server.close(force: true).then((status){
+  Future close() {
+    return server.close(force: true).then((status) {
         print('小程序服务已关闭 $status');
         return status;
     }); // 关闭http服务器
