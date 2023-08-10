79372d4d68d3c0cf3c9c8744c3db7fe0c6b2f0b7
Abner <0x23fd5097@gmail.com>
2023-08-10 18:24:51 +0800

    Update test.dart
    Signed-off-by: Abner <0x23fd5097@gmail.com>

--- lib/api/test.dart
+++ lib/api/test.dart
@@ -1,9 +1,7 @@
-import 'package:meta/meta.dart';
 import '../plugins/http.dart';
 
 final api = Http(basePath: '/api')();
 
-@immutable
 abstract class Test {
   /// 使用示例：
   ///  Test.test.then((r){
