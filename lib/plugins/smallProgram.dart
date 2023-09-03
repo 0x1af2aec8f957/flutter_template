75ba0f40316c6e12a46eaad17e9a2d2ae1b1b591
Abner <0x23fd5097@gmail.com>
2023-09-03 23:10:49 +0800

    feat: 小程序支持在App中进行强缓存

--- lib/plugins/smallProgram.dart
+++ lib/plugins/smallProgram.dart
@@ -1,7 +1,11 @@
 /// H5小程序
 import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
 // import 'dart:isolate';
 import 'package:dio/dio.dart';
+import 'package:mime/mime.dart';
+import 'package:crypto/crypto.dart';
 import 'package:archive/archive.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -10,10 +14,13 @@ import 'package:path/path.dart' as path;
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_router/shelf_router.dart' as router;
-import 'package:shelf_static/shelf_static.dart'; // shelf静态文件中间件
-import 'package:shelf_proxy/shelf_proxy.dart'; // shelf代理中间件
+import 'package:shelf_static/shelf_static.dart'; // shelf 静态文件中间件
+import 'package:shelf_proxy/shelf_proxy.dart'; // shelf 代理中间件
+import 'package:belatuk_range_header/belatuk_range_header.dart'; // RangeHeader 解析
 
 import '../../utils/dialog.dart';
+
+typedef CheckCache = bool Function(shelf.Request request); // Type: 检查是否需要缓存
 
 final _http = Dio()
   ..interceptors.add( // 打印api请求log
@@ -34,6 +41,12 @@ class SmallProgram { // isolate 启动参数
     final dir = Directory(path.join(_dir.path, 'downloads')); // 下载目录
     print('远程ZIP资源包下载目录：${dir.path}');
     if (!dir.existsSync()) dir.createSync(recursive: true); // 如果下载目录不存在，则创建
+    return dir;
+  });
+  Future<Directory> get cachesDirectory => applicationDirectory.then((_dir) { // 小程序资源缓存目录
+    final dir = Directory(path.join(_dir.path, 'caches')); // 缓存目录
+    print('小程序资源缓存目录：${dir.path}');
+    if (!dir.existsSync()) dir.createSync(recursive: true); // 如果缓存目录不存在，则创建
     return dir;
   });
 
@@ -118,7 +131,6 @@ class SmallProgram { // isolate 启动参数
     .catchError((error) {
       print('下载更新包失败：$error');
       Talk.alert('下载更新包失败');
-      return Future.error(error);
     });
 
     print('下载完成，下载资源包保存在：${tmpFile.path}');
@@ -146,6 +158,7 @@ class SmallProgram { // isolate 启动参数
     final handler = const shelf
       .Pipeline()
       .addMiddleware(shelf.logRequests()) // 打印信息
+      .addMiddleware(cacheMiddleware((shelf.Request request) => request.url.path.startsWith('image/'))) // 缓存中间件
       .addHandler(routes) // 代理访问处理器
     ;
 
@@ -156,4 +169,102 @@ class SmallProgram { // isolate 启动参数
       return _server;
     });
   }
+
+  shelf.Middleware cacheMiddleware(CheckCache? checkCache/* 除了基础的 cache-control 以外的缓存判断方法 */) => (shelf.Handler innerHandler) {
+    const requestCacheControls = ['only-if-cached']; // 需要缓存的请求指令集
+    const requestNoCacheControls = ['no-store', 'no-cache']; // 不需要缓存的请求指令集
+    const responseCacheControls = ['public', 'private']; // 需要缓存的响应指令集
+    const streamMediaMimeTypes = ['video', 'audio']; // 需要下载的文件类型
+
+    return (shelf.Request request) async {
+      final mimeType = lookupMimeType(request.url.path); // 获取请求文件类型
+      final isStreamMediaMimeTypes = mimeType != null && streamMediaMimeTypes.any(mimeType.startsWith); // 是否是流媒体文件
+      final requestHeaders = Map<String, String>.from(request.headers); // 请求头
+      final isNoCache = request.headersAll['Cache-Control']?.any(requestNoCacheControls.contains) ?? false; // 是否强制不需要缓存
+      final isAgentCache = (checkCache?.call(request) ?? false) && !isNoCache; // 是否需要缓存
+      final dir = await cachesDirectory;  // 获取缓存目录
+      final file = File(path.join(dir.path, '${md5.convert(request.url.toString().codeUnits)}.cache')); // 缓存文件
+      final header = File(path.join(dir.path, '${md5.convert(request.url.toString().codeUnits)}.header')); // 缓存头文件
+
+      if (isAgentCache && isStreamMediaMimeTypes && !file.existsSync()) { // TODO 音视频文件 在设置 range='bytes=0-' 的情况下，也不会返回全部内容，需要使用下载获取文件所有内容
+        await _http.downloadUri(
+          request.requestedUri.replace(scheme: serverAddress.scheme, host: serverAddress.host, port: serverAddress.port),
+          file.path,
+        ).then((value) {value.headers.map;
+          print('缓存主体文件下载完成[midea-size -> ${file.readAsBytesSync().length}]：${file.path}');
+        });
+      }
+
+      if (file.existsSync() && isAgentCache) { // 如果缓存文件存在，则直接返回
+        final headers = header.existsSync() ? Map<String, Object>.from(jsonDecode(header.readAsStringSync())) : Map<String, Object>();
+        final body = file.readAsBytesSync();
+        print('缓存url地址：${request.url}');
+        print('找到需要返回的缓存主体文件：${file.path}');
+        print('关联的缓存头文件路径：${header.path}');
+        final rangeHeader = RangeHeader.parse(requestHeaders['range'] ?? 'bytes=0-');
+        final rangeHeaderStart = rangeHeader.items.last.start;
+        final rangeHeaderEnd = rangeHeader.items.last.end == -1 ? body.length : rangeHeader.items.last.end;
+
+        if (rangeHeader.rangeUnit != 'bytes') return shelf.Response.badRequest(body: 'rangeUnit 仅支持 bytes');
+        if (rangeHeader.items.length > 1) return shelf.Response.badRequest(body: '不支持 Multipart ranges');
+
+        headers['content-length'] = (rangeHeaderEnd - rangeHeaderStart - 1).toString(); // 添加 Content-Length 响应头
+        headers['accept-ranges'] = rangeHeader.rangeUnit!; // 添加 Accept-Ranges 响应头
+        headers['content-range'] = '${rangeHeader.rangeUnit} ${rangeHeaderStart}-${rangeHeaderEnd - 1}/${body.length}'; // 添加 Content-Range 响应头
+        return shelf.Response(isStreamMediaMimeTypes ? 206/* 安卓设备 range 请求返回 200 会导致无法正常播放 */ : 200, body: body.sublist(rangeHeaderStart, rangeHeaderEnd), headers: headers);
+      }
+
+      requestHeaders['referer'] = 'shelf'; // 请求头中修改 referer 字段
+      requestHeaders['host'] = 'shelf'; // 请求头中修改 host 字段
+      requestHeaders['user-agent'] = 'shelf'; // 请求头中修改 user-agent 字段
+      requestHeaders['range'] = 'bytes=0-'; // 请求头中修改 Range 字段(请求整个文件)
+      requestHeaders['accept-ranges'] = 'bytes'; // 请求头中修改 Accept-Ranges 字段(请求整个文件)
+      // requestHeaders.remove('range');
+
+      return Future.sync(() => innerHandler(request.change(headers: requestHeaders))).then((response) async {
+        final _requestCacheControls = request.headersAll['Cache-Control'] ?? []; // 实际请求指令集
+        final _responseCacheControls = response.headersAll['Cache-Control'] ?? []; // 实际响应指令集
+        final isCache = responseCacheControls.any(_responseCacheControls.contains) || requestCacheControls.any(_requestCacheControls.contains); // 是否需要缓存
+
+        if (!isCache) return response; // 如果不需要缓存，则直接返回
+        final _completer = new Completer<List<int>>();
+        final headers = Map<String, String>()..addAll(response.headers); // 提取将响应头
+        final resultBytes = <int>[]; // 响应主体内容
+
+        headers['server'] = 'shelf'; // 添加 server 响应头
+        print('正在创建缓存头文件：${header.path}');
+        header.writeAsStringSync(jsonEncode(headers)); // 将响应头写入缓存头文件(jsonString)
+
+        final controller = response.read().listen(
+          (newBytes) {
+            print('正在接收缓存数据：${file.path}');
+            resultBytes.addAll(newBytes);
+            if (resultBytes.length != response.contentLength) return;
+
+            print('接收缓存数据完成：${request.url}}-${resultBytes.length}');
+            _completer.complete(resultBytes); // 手动完成，否则视频文件会一直等待
+          },
+          onDone: () => _completer.complete(resultBytes),
+          onError: _completer.completeError,
+          cancelOnError: true,
+        );
+
+        return _completer.future.then((result) {
+         
+          print('正在创建缓存主体文件：${file.path}');
+          file.writeAsBytesSync(resultBytes);
+          print('创建缓存主体文件写入完成：${file.path}');
+          controller.cancel(); // 取消订阅
+          return shelf.Response.ok(result, headers: headers, encoding: response.encoding, context: response.context);
+        });
+
+        // return response.read().reduce((previous, element) => [...previous, ...element]).then((result) { // TODO: 视频文件不会调用 reduce 方法，reduce 方法依赖 listen 中的 onDone 方法
+        //   print('正在创建缓存主体文件：${file.path}');
+        //   file.writeAsBytesSync(result); // 将响应内容写入缓存文件(bytes)
+        //   print('创建缓存主体文件写入完成：${file.path}');
+        //   return shelf.Response.ok(result, headers: response.headers, encoding: response.encoding, context: response.context);
+        // });
+      });
+    };
+  };
 }
