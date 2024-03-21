4c80f3b4d0d0308bc108e2ecc505721ee823935e
Abner <0x23fd5097@gmail.com>
2024-03-21 09:53:58 +0800

    style: 优化 models

--- lib/models/global.dart
+++ lib/models/global.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class GlobalModel extends ChangeNotifier{ // 全局的model
+import '../setup/config.dart';
+import '../setup/router.dart';
+import '../plugins/dialog.dart';
+
+class GlobalModel extends ChangeNotifier { // 全局的model
   GlobalModel(){ // 需要全局初始化的数据在此处初始化，按需初始化。
     // 不需要关心返回状态的才能在此处初始化, 需要关心状态的在lib/main.dart中的FutureBuilder处理
     initData();
   }
   // state
   bool isInitLoading = false; // 是否正在初始化数据
+  BuildContext get context => AppConfig.navigatorContext; // 全局 context
 
   // action
   Future<void> initData() { // 切换语言需要请求接口
